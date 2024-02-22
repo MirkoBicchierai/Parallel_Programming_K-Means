0716c7e1f68114e9dfa988d5c73b9341a8358d1f
mirko <bicchierai.mirko.developer@gmail.com>
2024-02-22 14:30:47 +0100

    Some fix

--- commonFunction.cpp
+++ commonFunction.cpp
@@ -3,70 +3,57 @@
 #include <sstream>
 #include <string>
 #include <cmath>
+
 using namespace std;
 
 struct Point{
-    int actualCentroid{};
-    vector<double> coordinate;
+    std::vector<double> cord;
+    int actualCentroid;
 };
+
+vector<Point> randomCentroid(int k, vector<Point> &data) {
+    vector<Point> centroids(k, {0, 0, 0});
+    for (int i = 0; i < k; ++i) {
+        centroids[i] = data[rand() % data.size()];
+    }
+    return centroids;
+}
 
 double euclideanDistance(Point p1, Point p2) {
     double dist = 0;
-    for(int i =0; i<p1.coordinate.size();i++)
-        dist += pow(p2.coordinate[i]-p1.coordinate[i],2);
+    dist += pow(p2.x-p1.x,2);
+    dist += pow(p2.y-p1.y,2);
+    dist += pow(p2.z-p1.z,2);
     return sqrt(dist);
 }
 
-bool areEqual(const std::vector<Point>& vec1, const std::vector<Point>& vec2) {
-    for(int j = 0; j<vec1.size(); j++){
-        for (int i = 0; i < vec1[j].coordinate.size(); i++) {
-            if (vec1[j].coordinate[i] != vec2[j].coordinate[i]) {
-                return false;
-            }
-        }
+bool areEqual(const std::vector<Point>& p1, const std::vector<Point>& p2) {
+    if (p1.size() != p2.size())
+        return false;
+    for (int i = 0; i < p1.size(); i++) {
+        if (p1[i].x != p2[i].x || p1[i].y != p2[i].y || p1[i].z != p2[i].z)
+            return false;
     }
     return true;
 }
-
-vector<Point> allZerosCentroid(int k, int dimension) {
-    vector<Point> centroids(k);
-    for (int i = 0; i < k; i++) {
-        for(int j = 0; j<dimension;j++)
-            centroids[i].coordinate.push_back(0);
-    }
-    return centroids;
-}
-
-vector<Point> randomCentroid(int k, int dimension, vector<Point>& data) {
-    vector<Point> centroids(k);
-    for (int i = 0; i < k; i++) {
-        for(int j = 0; j<dimension;j++)
-            centroids[i].coordinate.push_back(data[rand() % data.size()].coordinate[j]);
-    }
-    return centroids;
-}
-
 
 vector<Point> loadDataset(const string& path){
 
     vector<Point> data;
     ifstream file(path);
     string line;
-    getline(file, line);
     while (getline(file, line)) {
         stringstream ss(line);
         string temp;
         Point p{};
         getline(ss, temp, ',');
-        double latitude = stod(temp);
-        p.coordinate.push_back(latitude);
-
+        double x =  stod(temp);
+        p.x = x;
         getline(ss, temp, ',');
-        double longitude = stod(temp);
-        p.coordinate.push_back(longitude);
-
+        p.y = stod(temp);
+        getline(ss, temp, ',');
+        p.z = stod(temp);
         p.actualCentroid = -1;
-
         data.push_back(p);
     }
 
@@ -78,10 +65,12 @@ void writeCSV(const vector<Point>& data, const string& filename) {
     ofstream file(filename);
 
     for (const auto& point : data) {
-        for(double c : point.coordinate){
-            file << c;
-            file << ",";
-        }
+        file << point.x;
+        file << ",";
+        file << point.y;
+        file << ",";
+        file << point.z;
+        file << ",";
         file << point.actualCentroid;
         file << endl;
     }
